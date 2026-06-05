4d2f321075daa5c62f3db40548ab9bd313ed418f
kanishkasingh111 <singhkanishka228@gmail.com>
2026-06-05 17:22:34 +0530

    Day_5

---
+++ Week_1/Day_5/Reverse_string.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i=0;
+        int j=s.size()-1;
+        while(i<j)
+        {
+            swap(s[i],s[j]);
+            i++;
+            j--;
+        }
+    }
+};
+
+int main()
+{
+    vector<char>s={'h','e','l','l','o'};
+
+    Solution obj;
+    obj.reverseString(s);
+    for(char c:s)
+    cout<<c<<" ";
+
+    return 0;
+}
