83c1dc61230415681cd99794808e1170ea9a8172
kanishkasingh111 <singhkanishka228@gmail.com>
2026-06-09 22:36:06 +0530

    Day_2

---
+++ Week_2/Day_1/Day_2/Longest_Substr.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string S) {
+        vector<bool>count(256,0);
+        int first=0,second=0,len=0;
+        while(second<S.size())
+        {
+            // Repating char
+            while(count[S[second]])
+            {
+                count[S[first]]=0;
+                first++;
+            }
+            count[S[second]]=1;
+            len=max(len,second-first+1);
+            second++;
+        }
+
+        return len;
+    }
+};
+
+int main()
+{
+    string s;
+    cout<<"Enter a  string : ";
+    cin>>s;
+
+    Solution obj;
+    int ans=obj.lengthOfLongestSubstring(s);
+
+    cout<<"Longest Substring length: "<<ans<<endl;
+
+    return 0;
+
+}
