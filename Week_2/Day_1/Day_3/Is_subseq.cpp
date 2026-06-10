405c7228e3ea55fba5f0693ebfaa5a8867af31f4
kanishkasingh111 <singhkanishka228@gmail.com>
2026-06-10 23:06:33 +0530

    Day_3

---
+++ Week_2/Day_1/Day_3/Is_subseq.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0;
+        int j=0;
+        while(i<s.size() && j<t.size())
+        {
+            if(s[i]==t[j])
+            {
+                i++;
+                j++;
+            }
+            else
+            j++;
+        }
+        if(i==s.size())  return true;
+        else 
+        return false;
+    }
+};
+
+int main()
+{
+    string s,t;
+
+    cout<<"Enter string s: ";
+    cin>>s;
+
+    cout<<"Enter string t: ";
+    cin>>t;
+
+    Solution obj;
+
+    if(obj.isSubsequence(s,t))
+    cout<<"True"<<endl;
+    else
+    cout<<"False"<<endl;
+
+    return 0;
+
+}
